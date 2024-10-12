bb7bfc11e393dcdb2a101406251bf6bd5a847ee9
R. Kent James <kent@caspia.com>
2024-10-12 13:55:07 -0700

    Remove lines disabling output

--- ros_webdata/debian_packages.py
+++ ros_webdata/debian_packages.py
@@ -7,13 +7,8 @@ DEBIAN_URL = 'https://packages.debian.org/stable/allpackages?format=txt.gz'
 def get_debian_packages():
     print('get_debian_packages')
     response = requests.get(DEBIAN_URL)
-    print(f'response encoding is {response.encoding}')
-    print(type(response.content))
-    print(':'.join(hex(x)[2:] for x in (response.content[:200])))
     # content = response.content.decode('utf-8')
     content = response.text
-    print(content[:200])
-    print(f'content:\n{len(content)}')
     line_count = 0
     packages = {}
     for line in content.split('\n'):
@@ -26,9 +21,6 @@ def get_debian_packages():
         line_count += 1
         package_name = line[:left_paren - 1]
         package_desc = line[right_paren + 2:]
-        s4pane = '4pane'
-        print(f'{package_name}: {package_desc}')
-        # packages[package_name] = package_desc
-        # For some reason, this prints as gibberish in github actions
+        packages[package_name] = package_desc
         # print(f'{package_name} : {package_desc}')
     return packages
