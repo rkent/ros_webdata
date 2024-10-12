6c0949cc6c0ba78d727e334674513f19ae5a0e4a
R. Kent James <kent@caspia.com>
2024-10-12 14:10:55 -0700

    Stop limiting debian linecount

--- ros_webdata/debian_packages.py
+++ ros_webdata/debian_packages.py
@@ -12,13 +12,13 @@ def get_debian_packages():
     line_count = 0
     packages = {}
     for line in content.split('\n'):
-        if line_count> 20:
-            break
+        # if line_count> 20:
+        #    break
+        line_count += 1
         left_paren = line.find('(')
         right_paren = line.find(')')
         if left_paren < 0 or right_paren < 0:
             continue
-        line_count += 1
         package_name = line[:left_paren - 1]
         package_desc = line[right_paren + 2:]
         packages[package_name] = package_desc
